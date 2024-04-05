2eae73705a3df4ae60a5a152f753c5494410ad65
iamLiwansh <saini.liwansh@gmail.com>
2024-04-05 12:41:55 +0530

    updated email sender

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -44,7 +44,7 @@ export const getPayloadClient = async ({initOptions}: Args ={}): Promise<Payload
         cached.promise =payload.init({
             email: {
                 transport: transporter,
-                fromAddress: "liwanshsaini123456789@gmail.com",//if in case doesnt work use onboarding@resend.com
+                fromAddress: "onboarding@resend.com",//if in case doesnt work use onboarding@resend.com
                 fromName: "DigitalHippo"
             },
             secret:process.env.PAYLOAD_SECRET,
