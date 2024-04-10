28106353fdf086a5d44744abe4b3e51142547c46
iamLiwansh <saini.liwansh@gmail.com>
2024-04-10 17:19:25 +0530

    created upload function

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,4 +1,4 @@
-import { PRODUCT_CATEGORIES } from "@/config";
+import { PRODUCT_CATEGORIES } from "../../config";
 import {CollectionConfig} from "payload/types";
 
 export const Products : CollectionConfig = {
@@ -50,7 +50,76 @@ export const Products : CollectionConfig = {
         required: true,
         relationTo: "product_files",
         hasMany:false,
-    }
+    },
+    {
+        name: "approvedForSale",
+        label: "Product Status",
+        type: "select",
+        defaultValue:"pending",
+        access: {
+            create: ({req}) => req.user.role === "admin",
+            read: ({req}) => req.user.role === "admin",
+            update: ({req}) => req.user.role === "admin",
+        },
+        options:[
+            {
+                label: "Pending verfication",
+                value: "Pending"
+            },
+            {
+                label: "Approved",
+                value : "approved",
+            },
+            {
+                label: "Denied",
+                value: "denied",
+            },
+        ],
+    },
+    {
+        name : "priceId",
+        access: {
+            create: () => false,
+            read: ()=> false,
+            update: ()=> false
+        },
+        type: 'text',
+        admin: {
+            hidden: true,
+        },
+    },
+    {
+        name : "stripeId",
+        access: {
+            create: () => false,
+            read: ()=> false,
+            update: ()=> false
+        },
+        type: 'text',
+        admin: {
+            hidden: true,
+        },
+    },
+    {
+        name: "images",
+        type: 'array',
+        label: "Product images",
+        minRows: 1,
+        maxRows: 4,
+        required: true,
+        labels: {
+            singular: "Image",
+            plural: "Images"
+        },
+        fields: [
+            {
+            name: "image",
+            type: "upload",
+            relationTo: 'media',
+            required: true,
+        },
+    ],
+    },
 ],//who can acces which part(autharization)
        
     
