68ac265818a02b5b92592a02f591b62aad1c11e0
iamLiwansh <saini.liwansh@gmail.com>
2024-04-21 21:41:45 +0530

    product listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import  ProductReel  from "../components/ProductReel";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -41,7 +42,7 @@ export default function Home() {
       </div>
     </div>
 
-    {/* {TODO: List products} */}
+    <ProductReel query={{sort:'desc' , limit:4}} href='/products' title="Brand new"/>
   </MaxWidthWrapper>
 
   <section className="border-t border-gray-200 bg-gray-50">
