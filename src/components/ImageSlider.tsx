45bdf54dd28da44215969c99756d592d187b5c07
iamLiwansh <saini.liwansh@gmail.com>
2024-04-22 13:06:39 +0530

    adding image slider

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import {Swiper , SwiperSlide} from "swiper/react"
+
+interface ImageSliderProps{
+    urls: string[]
+}
+const ImageSlider = ({urls}: ImageSliderProps) => {
+
+    const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300"
+    const inactiveStyles = "hidden text-gray-400"
+
+    return <div className="group realtive bg-zinc-100 aspect-square overflow-hidden rounded-xl">
+        <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
+            <button></button>
+            <button></button>
+        </div>
+        <Swiper className="h-full w-full">
+            {urls.map((url,i) => (
+                <SwiperSlide key={i} className="-z-10 relative h-full w-full">
+                    <Image fill loading = 'eager' className="-z-10 h-full w-full object-cover object-center"
+                    src={url}
+                    alt = 'Product image'/>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
+}
+
+export default ImageSlider
--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -5,6 +5,7 @@ import { Skeleton } from "./ui/skeleton"
 import Link from "next/link"
 import { cn, formatPrice } from "../lib/utils"
 import { PRODUCT_CATEGORIES } from "@/config"
+import ImageSlider from "./ImageSlider"
 
 interface ProductListingProps {
     product : Product | null
@@ -28,6 +29,8 @@ const ProductListing = ({product , index}: ProductListingProps) => {
         ({value}) => value === product.category
     )?.label
 
+    const validUrls = product.images.map(({image}) => (typeof image === "string"?image : image.url)).filter(Boolean) as string[] 
+
     if(isVisible && product){
         return (
             <Link 
@@ -38,6 +41,8 @@ const ProductListing = ({product , index}: ProductListingProps) => {
             )}
             href={`/product/${product.id}`}>
                 <div className="flex flex-col w-full">
+                    <ImageSlider urls={validUrls}/>
+
                     <h3 className="mt-4 font-medium text-sm text-gray-700">{product.name}</h3>
                     <p className="mt-1 text-sm text-gray-500">{label}</p>
                     <p className="mt-1 font-medium text-sm text-gray-900">{formatPrice(product.price)}</p>
