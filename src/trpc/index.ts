eafe0722a007551d919e53bc227449f26f4a56f7
iamLiwansh <saini.liwansh@gmail.com>
2024-04-24 13:02:47 +0530

    payment and thankyou page

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,9 +3,11 @@ import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
 import { QueryValidator } from "../lib/query-validator";
 import { getPayloadClient } from "../get-payload";
+import { paymentRouter } from "./payment-router";
 
 export const appRouter = router({
     auth : authRouter,
+    payment: paymentRouter
 
     getinfiniteProducts: publicProcedure.input(z.object({
         limit: z.number().min(1).max(100),
