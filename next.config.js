32d26dafc8129223f795978c59715041ee6621d6
iamLiwansh <saini.liwansh@gmail.com>
2024-05-03 15:36:05 +0530

    updated the domains

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
-			},
+		domains: [
+			'localhost',
+			'digitalhippo-production.up.railway.app',
 		],
 	},
 };
